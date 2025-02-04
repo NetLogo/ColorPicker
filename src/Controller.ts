2bc31f4d26839a3fa45d3795dbe43a86e0074563
TheBizzle <jason.bertsche@gmail.com>
2025-02-04 09:15:55 -0600

    Refactor: Serve directories more cleanly
    As less verbosely/repetitively

--- src/Controller.ts
+++ src/Controller.ts
@@ -8,33 +8,20 @@ const __dirname_ = path.dirname(fileURLToPath(import.meta.url))
 
 const app: Express = express()
 
+const serveDirAs = (dirName: string, urlName: string, fileExtension: string): void => {
+  app.get(`/${urlName}/*.${fileExtension}`, (req: Request, res: Response) => {
+    const relativePath = req.path.replace(new RegExp(`^\/${urlName}\/`), "")
+    res.sendFile(path.resolve(`${__dirname_}/../../${dirName}/${relativePath}`))
+  })
+}
+
 app.get("/", (_: Request, res: Response) => {
   res.sendFile(path.resolve(`${__dirname_}/../../html/index.html`))
 })
 
-app.get("/stylesheets/main.css", (_: Request, res: Response) => {
-  res.sendFile(path.resolve(`${__dirname_}/../../stylesheets/main.css`))
-})
-
-app.get("/stylesheets/main/main.css", (_: Request, res: Response) => {
-  res.sendFile(path.resolve(`${__dirname_}/../../stylesheets/main.css`))
-})
-
-app.get("/javascripts/Main.js", (_: Request, res: Response) => {
-  res.sendFile(path.resolve(`${__dirname_}/../typescripts/Main.js`))
-})
-
-app.get("/javascripts/ColorModel.js", (_: Request, res: Response) => {
-  res.sendFile(path.resolve(`${__dirname_}/../typescripts/ColorModel.js`))
-})
-
-app.get("/javascripts/DOM.js", (_: Request, res: Response) => {
-  res.sendFile(path.resolve(`${__dirname_}/../typescripts/DOM.js`))
-})
-
-app.get("/deps/nl-color-model.js", (_: Request, res: Response) => {
-  res.sendFile(path.resolve(`${__dirname_}/../../deps/nl-color-model.js`))
-})
+serveDirAs(     "stylesheets", "stylesheets", "css")
+serveDirAs("dist/typescripts", "javascripts",  "js")
+serveDirAs(            "deps",        "deps",  "js")
 
 const port     = 8007
 const onLaunch = (): void => { console.log(`Server is running at https://localhost:${port}`) }
